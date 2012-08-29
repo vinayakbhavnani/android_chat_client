3502c8af5afa5a5878ddb4aa8f3823e8c2e8e014
Vinayak Bhavnani <vinayak.bhavnani@gmail.com>
2012-08-29 17:21:21 +0530

    new directory structure

---
+++ src/directi/androidteam/training/chatclient/Util/ConnectionHandler.java
@@ -0,0 +1,12 @@
+package directi.androidteam.training.chatclient.Util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vinayak
+ * Date: 29/8/12
+ * Time: 4:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConnectionHandler {
+
+}
