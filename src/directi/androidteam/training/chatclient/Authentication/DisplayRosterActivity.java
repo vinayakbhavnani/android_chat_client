0656665efc1c369a9d22facd6aa043f2cbbd44a9
rajat <rajat@rajat-ThinkPad-X220.(none)>
2012-09-04 00:02:20 +0530

    This commit contains the complete source code for front end as well as backend for login .. it has been tested with message sending and is working ... the following things need to be done though .. *refactoring of code (including using tags to generate xml stanzas ... *move Base64.java to util/lib ... *move DisplayRosterActivity to Roster/sumit's working folder ... *remove message sending code from ConnectGTalk.java ... *merge ConnectGTalk.java with Test.java (Test.java is redundant)

---
+++ src/directi/androidteam/training/chatclient/Authentication/DisplayRosterActivity.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rajat
+ * Date: 9/3/12
+ * Time: 1:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DisplayRosterActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.roster);
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(LoginActivity.USERNAME);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Welcome " + username);
+        setContentView(textView);
+    }
+}
