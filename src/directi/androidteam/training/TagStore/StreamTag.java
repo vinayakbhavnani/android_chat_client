c5749153398af7ee610479fb94953fccd4cb5bb8
Vinayak Bhavnani <vinayak.bhavnani@gmail.com>
2012-09-05 12:49:47 +0530

    packet reader and writer

---
+++ src/directi/androidteam/training/TagStore/StreamTag.java
@@ -0,0 +1,24 @@
+package directi.androidteam.training.TagStore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rajat
+ * Date: 9/4/12
+ * Time: 1:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StreamTag extends Tag {
+    public StreamTag(String tagname, String to, String XMLNameSpace, String XMLNameSpaceStream, String version) {
+        this.tagname=tagname;
+        attributes = new HashMap<String, String>();
+        attributes.put("to", to);
+        attributes.put("xmlns",XMLNameSpace);
+        attributes.put("xmlns:stream", XMLNameSpaceStream);
+        attributes.put("version", version);
+        childTags = new ArrayList<Tag>();
+        content=null;
+    }
+}
