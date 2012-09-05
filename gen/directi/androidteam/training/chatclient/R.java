66a715f5b711a5d356d887bc7f0703fd88c4010a
ssumit <singhsumitbit@gmail.com>
2012-09-05 13:50:59 +0530

    added some strings

--- gen/directi/androidteam/training/chatclient/R.java
+++ gen/directi/androidteam/training/chatclient/R.java
@@ -15,8 +15,10 @@ public final class R {
     }
     public static final class id {
         public static final int btnSend=0x7f050003;
+        public static final int button=0x7f050007;
         public static final int linearLayout1=0x7f050001;
         public static final int password=0x7f050005;
+        public static final int text=0x7f050006;
         public static final int textInput=0x7f050002;
         public static final int textOutput=0x7f050000;
         public static final int username=0x7f050004;
@@ -30,6 +32,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int button_login=0x7f040003;
         public static final int password=0x7f040002;
+        public static final int start_chatting=0x7f040004;
         public static final int username=0x7f040001;
     }
 }
