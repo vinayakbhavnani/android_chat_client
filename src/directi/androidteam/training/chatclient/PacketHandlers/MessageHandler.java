8ab1f61f3cda1e68a7c9b161e6f408e4c0a5aa48
Vinayak Bhavnani <vinayak.bhavnani@gmail.com>
2012-09-06 19:14:43 +0530

    singleton messagehandler

--- src/directi/androidteam/training/chatclient/PacketHandlers/MessageHandler.java
+++ src/directi/androidteam/training/chatclient/PacketHandlers/MessageHandler.java
@@ -3,6 +3,9 @@ package directi.androidteam.training.chatclient.PacketHandlers;
 import android.util.Log;
 import directi.androidteam.training.StanzaStore.MessageStanza;
 import directi.androidteam.training.TagStore.Tag;
+import directi.androidteam.training.chatclient.Chat.ChatBox;
+
+import java.util.HashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,6 +15,16 @@ import directi.androidteam.training.TagStore.Tag;
  * To change this template use File | Settings | File Templates.
  */
 public class MessageHandler implements Handler{
+
+    HashMap<String,ChatBox> chatpanes;
+    private static final MessageHandler messageHandler = new MessageHandler();
+    private MessageHandler(){
+
+    }
+
+    public static MessageHandler getInstance(){
+        return messageHandler;
+    }
 
     @Override
     public void processPacket(Tag tag){
