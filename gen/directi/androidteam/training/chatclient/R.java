24873f269e06d38b20335ab788571acd3262d935
Rajat <rajat@test.com>
2012-09-11 20:17:17 +0530

    some more changes to login ui

--- gen/directi/androidteam/training/chatclient/R.java
+++ gen/directi/androidteam/training/chatclient/R.java
@@ -15,19 +15,13 @@ public final class R {
         public static final int login_button=0x7f020001;
     }
     public static final class id {
-<<<<<<< HEAD
         public static final int button=0x7f05000b;
-        public static final int password=0x7f050008;
-        public static final int progress_bar=0x7f050009;
-        public static final int text=0x7f05000a;
-=======
-        public static final int button=0x7f05000a;
         public static final int chatlist=0x7f050003;
         public static final int message=0x7f050005;
         public static final int password=0x7f050008;
+        public static final int progress_bar=0x7f050009;
         public static final int sendmessage=0x7f050006;
-        public static final int text=0x7f050009;
->>>>>>> 997fe836630b74362c60c4d6f07ccf45a9da77e4
+        public static final int text=0x7f05000a;
         public static final int username=0x7f050007;
         public static final int widget33=0x7f050000;
         public static final int widget36=0x7f050001;
