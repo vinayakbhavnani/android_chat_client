fb8e956d49452f983955de10fec030558c637ec6
ssumit <singhsumitbit@gmail.com>
2012-09-20 18:54:21 +0530

    roster ui improved again

--- src/directi/androidteam/training/chatclient/Roster/DisplayRosterActivity.java
+++ src/directi/androidteam/training/chatclient/Roster/DisplayRosterActivity.java
@@ -81,7 +81,8 @@ public class DisplayRosterActivity extends Activity {
         BitmapDrawable result = new BitmapDrawable(scaledBitmap);
         view.setImageDrawable(result);
         view.setScaleType(ImageView.ScaleType.FIT_START);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) view.getLayoutParams();
+        //LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) view.getLayoutParams();
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
         params.width = width;
         params.height = height;
         view.setLayoutParams(params);
