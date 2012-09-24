6dea3db11242702a8fb027364e5a0e503bf66ee9
Rajat <rajat@test.com>
2012-09-24 13:27:22 +0530

    progress circle added

--- src/directi/androidteam/training/chatclient/Authentication/UserListActivity.java
+++ src/directi/androidteam/training/chatclient/Authentication/UserListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import directi.androidteam.training.chatclient.R;
@@ -35,11 +36,25 @@ public class UserListActivity extends ListActivity {
     public void onListItemClick(ListView view, View v, int position, long id) {
         String username = ((TextView)((RelativeLayout)v).getChildAt(0)).getText().toString();
         Log.d("Login Flow (Check Username)", username);
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progress_bar_users);
+        progressBar.setVisibility(View.VISIBLE);
         UserDatabaseHandler db = new UserDatabaseHandler(this);
         String password = db.getPassword(username);
         Log.d("Login Flow (Check Password)", password);
         db.close();
         (new ConnectGTalk(this)).execute(username, password);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progress_bar_users);
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     public void addUser(View view) {
