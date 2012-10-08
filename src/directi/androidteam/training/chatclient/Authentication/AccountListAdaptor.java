7b5aec0ad3a4eb23792598f0922ec7e1e0460f4f
Vinayak Bhavnani <vinayak.bhavnani@gmail.com>
2012-10-08 14:59:10 +0530

    ui for account list

---
+++ src/directi/androidteam/training/chatclient/Authentication/AccountListAdaptor.java
@@ -0,0 +1,22 @@
+package directi.androidteam.training.chatclient.Authentication;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import directi.androidteam.training.chatclient.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vinayak
+ * Date: 8/10/12
+ * Time: 2:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountListAdaptor extends ArrayAdapter<Account> {
+    public AccountListAdaptor(Context context , ArrayList<Account> accounts){
+        super(context, R.layout.accountitem,accounts);
+
+
+    }
+}
