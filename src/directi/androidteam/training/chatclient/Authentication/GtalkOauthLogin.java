a8c992cb9ef5bbc2c1728d6ba12c5df80680443a
Vinayak Bhavnani <vinayak.bhavnani@gmail.com>
2012-10-08 11:28:26 +0530

    gtalkSSO works

---
+++ src/directi/androidteam/training/chatclient/Authentication/GtalkOauthLogin.java
@@ -0,0 +1,11 @@
+package directi.androidteam.training.chatclient.Authentication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vinayak
+ * Date: 5/10/12
+ * Time: 12:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GtalkOauthLogin {
+}
---
+++ src/directi/androidteam/training/chatclient/Authentication/OnTokenAcquired.java
@@ -0,0 +1,11 @@
+package directi.androidteam.training.chatclient.Authentication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vinayak
+ * Date: 5/10/12
+ * Time: 1:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OnTokenAcquired {
+}
