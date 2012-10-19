bb4887fddcb48659706864d08b1ddcfe0f90cf79
Rajat <rajat@test.com>
2012-10-19 17:32:26 +0530

    some changes

---
+++ src/directi/androidteam/training/chatclient/Authentication/RosterFetchStatus.java
@@ -0,0 +1,11 @@
+package directi.androidteam.training.chatclient.Authentication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rajat
+ * Date: 10/19/12
+ * Time: 4:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RosterFetchStatus {
+}
