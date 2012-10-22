e44005f6946380dd6a2135472d0b23127cf77f43
Vineet Paliwal <vineet.p@directi.com>
2012-10-22 18:28:03 +0530

    Set Notification made Get Notification in TalkTo Notification Manager Fire Notification removed in TalkTo Notification Manager TalkNotifier getInstance(Context) method made synchronized instead of inner code block

--- src/directi/androidteam/training/chatclient/Notification/TalkToNotificationManager.java
+++ src/directi/androidteam/training/chatclient/Notification/TalkToNotificationManager.java
@@ -14,8 +14,6 @@ public class TalkToNotificationManager {
     private static NotificationManager notificationManager ;
 
     private Context notificationContext ;
-    public static String NOTIFICATION_ID = "NOTIFICATION_ID" ;
-    public static String NOTIFICATION_TYPE = "NOTIFICATION_TYPE";
 
     public TalkToNotificationManager(Context currentContext) {
         notificationContext = currentContext;
@@ -23,12 +21,12 @@ public class TalkToNotificationManager {
     }
 
     public void notify(TalkToNotification notification) {
-        NotificationCompat.Builder notificationBuilder = setNotification(notification.getIcon(), notification.getContentTitle(), notification.getContentText(), notification.getTickerText(), notification.getNotificationID(),notification.getTargetIntent(), notification.getHomeActivityClass());
-        fireNotification(notification.getNotificationID(),notificationBuilder);
-        Log.d("dispatch notification" , " successfully exiting dispatch notification");
+        Notification androidNotification = getNotification(notification.getIcon(), notification.getContentTitle(), notification.getContentText(), notification.getTickerText(), notification.getNotificationID(),notification.getTargetIntent(), notification.getHomeActivityClass());
+        notificationManager.notify("note"+notification.getNotificationID(),notification.getNotificationID(),androidNotification);
+        Log.d("dispatch notification" , " successfully exiting notify");
     }
 
-    private NotificationCompat.Builder setNotification ( int icon , String contentTitle , String contentText , String tickerText, int notificationID ,Intent targetIntent , Class homeActivityClass) {
+    private Notification getNotification ( int icon , String contentTitle , String contentText , String tickerText, int notificationID ,Intent targetIntent , Class homeActivityClass) {
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(notificationContext);
         notificationBuilder.setSmallIcon(icon);
         notificationBuilder.setTicker(tickerText );
@@ -38,13 +36,8 @@ public class TalkToNotificationManager {
         notificationBuilder.setWhen(System.currentTimeMillis());
         notificationBuilder.setAutoCancel(true);
         notificationBuilder.setContentIntent( TaskStackBuilder.create(notificationContext).addParentStack(homeActivityClass).addNextIntent(new Intent(notificationContext,homeActivityClass)).addNextIntent(targetIntent).getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT));
-        return notificationBuilder;
-    }
-
-    private void fireNotification(int notificationID,NotificationCompat.Builder notificationBuilder) {
         Notification notification = notificationBuilder.build();
-        notificationManager.notify("note"+notificationID,notificationID,notification);
-        Log.d("fire notification" , "successfully exiting fire notification");
+        return notification;
     }
 
 }
--- src/directi/androidteam/training/chatclient/Notification/TalkToNotifier.java
+++ src/directi/androidteam/training/chatclient/Notification/TalkToNotifier.java
@@ -14,13 +14,11 @@ public class TalkToNotifier {
           notificationManager = new TalkToNotificationManager(context);
     }
 
-    public static TalkToNotifier getInstance(Context context) {
-        synchronized(TalkToNotifier.class ) {
-            if(instance == null) {
-                 instance = new TalkToNotifier(context);
-            }
-        };
-            return instance;
+    public static synchronized TalkToNotifier getInstance(Context context) {
+        if(instance == null) {
+            instance = new TalkToNotifier(context);
+        }
+        return instance;
     }
 
     public void sendMessageNotification(String messageSender , String message )  {
