c67c6832585ace353c0baf711571a1c5440f7054
Vineet Paliwal <vineet.p@directi.com>
2012-10-24 14:52:12 +0530

    Message format changed . ArrayList parameterized and kept sorted .

--- src/directi/androidteam/training/chatclient/Notification/MessageNotificationHandler.java
+++ src/directi/androidteam/training/chatclient/Notification/MessageNotificationHandler.java
@@ -16,28 +16,35 @@ public class MessageNotificationHandler {
     private static int notificationID = 1 ;
     private Context context;
     private static final String LOGTAG = "MessageNotificationHandler";
-    private ArrayList senderList;
+    private ArrayList<String> senderList;
 
     public MessageNotificationHandler(Context context) {
         this.context = context;
-        senderList = new ArrayList();
+        senderList = new ArrayList<String>();
     }
 
     public TalkToNotification getNotification(String messageSender,String message ) {
         Intent targetIntent = new Intent(context,ChatBox.class);
         targetIntent.putExtra(ChatBox.BUDDY_ID, messageSender);
-        if(!senderList.contains(messageSender) ) {
-            senderList.add(messageSender);
+        int index =  senderList.indexOf(messageSender);
+        if( index != -1) {
+            senderList.remove(index);
         }
-        int times = senderList.size();
+        senderList.add(0,messageSender);
+        int numberOfContacts = senderList.size();
         String contentTitle;
-        if(times > 1) {
-             contentTitle = "New messages from " + times + " contacts" ;
-            message = "Last message from " + messageSender + " : " + message ;
-        }    else {
+        if(numberOfContacts > 1) {
+             contentTitle = "New messages from " + numberOfContacts + " contacts" ;
+            if(numberOfContacts == 2) {
+                message = messageSender + " , " + senderList.get(1);
+            } else {
+                int others = numberOfContacts - 2;
+                message = messageSender + " , " + senderList.get(1) + " and " + others + " other contacts " ;
+            }
+        }   else {
             contentTitle = messageSender;
         }
-        TalkToNotification notification = new TalkToNotification(targetIntent,DisplayRosterActivity.class,R.drawable.ic_launcher,contentTitle ,message,messageSender + " : " + message,notificationID,times);
+        TalkToNotification notification = new TalkToNotification(targetIntent,DisplayRosterActivity.class,R.drawable.ic_launcher,contentTitle ,message,messageSender + " : " + message,notificationID,numberOfContacts);
         Log.d(LOGTAG,"notification created successfully");
         return notification;
     }
