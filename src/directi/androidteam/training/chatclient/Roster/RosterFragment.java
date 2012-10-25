9955470aeae983670d7b5f17f673d8d2c923e120
Rajat <rajat@test.com>
2012-10-25 15:53:42 +0530

    shifted to branch unstable-tabs-fragmented and fixed minor issues

--- src/directi/androidteam/training/chatclient/Roster/RosterFragment.java
+++ src/directi/androidteam/training/chatclient/Roster/RosterFragment.java
@@ -3,7 +3,6 @@ package directi.androidteam.training.chatclient.Roster;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,32 +15,20 @@ import directi.androidteam.training.chatclient.R;
 
 import java.util.ArrayList;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ssumit
- * Date: 10/19/12
- * Time: 7:39 PM
- * To change this template use File | Settings | File Templates.
- */
 public class RosterFragment extends Fragment {
     private View view;
-    public RosterFragment() {
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        Log.d("ioio", "oncreate - roster fragment");
-        RosterManager.getInstance().setDisplayRosterActivity((GlobalTabActivity)getActivity());
+        RosterManager.getInstance().setGlobalTabActivity((GlobalTabActivity)getActivity());
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d("ioio", "rosterfragment oncreate view- acct fragment");
-        view = inflater.inflate(R.layout.roster,container,false);
+        view = inflater.inflate(R.layout.roster, container, false);
         ListView listView = ((ListView)view.findViewById(R.id.roster_list));
-        if(listView!=null) {
-            RosterItemAdapter rosterItemAdapter = new RosterItemAdapter(getActivity(), R.layout.rosterlistitem, new ArrayList<RosterItem>());
-            listView.setAdapter(rosterItemAdapter);
+        if(listView != null) {
+            listView.setAdapter(new RosterItemAdapter(getActivity(), R.layout.rosterlistitem, new ArrayList<RosterItem>()));
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -54,14 +41,11 @@ public class RosterFragment extends Fragment {
     }
 
     private void updateRosterList(ArrayList<RosterItem> rosterList) {
-        if(view==null)
-            return;
+        if(view == null) {return;}
         RosterItemAdapter rosterItemAdapter = ((RosterItemAdapter)(((ListView)view.findViewById(R.id.roster_list)).getAdapter()));
         rosterItemAdapter.setRosterItems(new ArrayList<RosterItem>(rosterList));
         rosterItemAdapter.notifyDataSetChanged();
     }
-
-
 
     public void onListItemClick(ListView view, View v, int position, long id) {
         RosterItem rosterItem = (RosterItem) view.getItemAtPosition(position);
@@ -71,5 +55,4 @@ public class RosterFragment extends Fragment {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
-
 }
